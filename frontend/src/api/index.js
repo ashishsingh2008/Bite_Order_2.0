b0aca733ca64feb83aeff5f9b6eb6e349b5da20c
ASHISH SINGH <155746118+ashishsingh2008@users.noreply.github.com>
2024-06-29 16:13:31 +0530

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://bite-order-2-0-1.onrender.com/api",
+  baseURL: "https://bite-order-2-0-1.onrender.com/api/",
 });
 
 //auth
